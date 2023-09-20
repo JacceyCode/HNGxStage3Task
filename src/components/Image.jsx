884feed5c73c1514c626a645b2e4f361d2d5b156
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-09-20 01:54:50 +0100

    implemented drag and drop

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,26 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
 /* eslint-disable react/prop-types */
 function Image({ image }) {
-  const { url, tag } = image;
+  const { url, tag, id } = image;
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
   return (
-    <img className="h-64 w-72 cursor-grab" src={url} alt={tag} loading="lazy" />
+    <img
+      className="h-64 w-72 cursor-grab"
+      ref={setNodeRef}
+      {...attributes}
+      style={style}
+      {...listeners}
+      src={url}
+      alt={tag}
+      loading="lazy"
+    />
   );
 }
 
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,10 +3,16 @@ import Image from "../components/Image";
 import { imageList } from "../imageList/ImageList";
 import Logo from "../components/Logo";
 import LoginBtn from "../components/LoginBtn";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 function User() {
   const [tag, setTag] = useState("");
-  const [filteredImageList, setFilteredImageList] = useState([]);
+  const [filteredImageList, setFilteredImageList] = useState(imageList);
   const [error, setError] = useState(false);
 
   function filterImage(e) {
@@ -14,14 +20,29 @@ function User() {
     setError(false);
     const newTag = tag.trim().toLocaleLowerCase();
     const filteredList = imageList.filter((image) => image.tag === newTag);
-    if (filteredList.length === 0) setError(true);
-    setFilteredImageList(filteredList);
+    filteredList.length === 0
+      ? setError(true)
+      : setFilteredImageList(filteredList);
     setTag("");
+  }
+
+  function onDragEnd(event) {
+    const { active, over } = event;
+    if (active.id === over.id) return;
+    setFilteredImageList((filteredImageList) => {
+      const oldIndex = filteredImageList.findIndex(
+        (image) => image.id === active.id,
+      );
+      const newIndex = filteredImageList.findIndex(
+        (image) => image.id === over.id,
+      );
+      return arrayMove(filteredImageList, oldIndex, newIndex);
+    });
   }
 
   return (
     <>
-      <header className="mb-2 flex flex-wrap items-center justify-between p-4">
+      <header className="mb-2 flex flex-wrap items-center justify-between px-8 py-4">
         <Logo />
 
         <LoginBtn to="/">Log Out</LoginBtn>
@@ -54,11 +75,18 @@ function User() {
         </section>
 
         <section className="grid grid-cols-1 place-items-center gap-6 p-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-          {filteredImageList.length > 0
-            ? filteredImageList.map((image) => (
+          <DndContext collisionDetection={closestCenter} onDragEnd={onDragEnd}>
+            <SortableContext
+              items={
+                filteredImageList.length > 0 ? filteredImageList : imageList
+              }
+              strategy={verticalListSortingStrategy}
+            >
+              {filteredImageList.map((image) => (
                 <Image image={image} key={image.id} />
-              ))
-            : imageList.map((image) => <Image image={image} key={image.id} />)}
+              ))}
+            </SortableContext>
+          </DndContext>
         </section>
       </section>
     </>
