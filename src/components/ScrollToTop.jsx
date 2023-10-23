a906ba857cbd607639c125b723613001328a87d9
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-10-23 08:32:51 +0100

    added scrollToTup Icon

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { FaAngleUp } from "react-icons/fa";
+
+function ScrollToTop() {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 750 ? setShowTopBtn(true) : setShowTopBtn(false);
+    });
+  }, []);
+
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      {showTopBtn && (
+        <FaAngleUp
+          className="fixed bottom-2 right-1 z-10 mx-auto h-8 w-8 animate-bounce cursor-pointer rounded-full border-4 border-black bg-red-700 hover:border-red-700 hover:bg-black hover:text-red-700 lg:right-8 lg:h-12 lg:w-12"
+          onClick={goToTop}
+        />
+      )}
+    </>
+  );
+}
+
+export default ScrollToTop;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 function AppLayout() {
   return (
     <section className="bg-[#d4d4d4]">
       <section className="mx-auto flex min-h-screen max-w-[120rem] flex-col items-center justify-center gap-4">
         <Outlet />
+
+        <ScrollToTop />
 
         <Footer />
       </section>
